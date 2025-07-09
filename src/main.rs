569272c343ef18a75036829b05e77e48cc836f5f
Raul Huertas <raul.huertas@mobilemoney.net>
2025-07-09 11:33:00 -0500

    tested the new partition size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!, adjusting partitions working correctly");
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    dotenvy::dotenv().expect("Unable to access .env file");
+    // build our application with a single route
+    let server_url = std::env::var("SERVER_ADDR").unwrap_or("localhost:3335".to_owned());
+
+
+    let mut res = reqwest::blocking::get(server_url+"/deviceCheckUpdates")?;
+
+    eprintln!("Response: {:?} {}", res.version(), res.status());
+    eprintln!("Headers: {:#?}\n", res.headers());
+
+    // copy the response body directly to stdout
+    res.copy_to(&mut std::io::stdout())?;
+
+    Ok(())
 }
+
